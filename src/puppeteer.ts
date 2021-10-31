3de2c2caea012fb8020ed7c53129ac1295075186
Mariano Benedettini <mbenedettini@gmail.com>
2021-10-30 23:00:44 -0300

    Add comment

--- src/puppeteer.ts
+++ src/puppeteer.ts
@@ -6,6 +6,19 @@ async function html2pdf(html: string, browserURL?: string) {
   let host = '';
   if (!browserURL) {
     let host = '';
+    /*
+      HACK because Chrome when using browserURL needs the Host header
+      to be localhost or an ip address.
+      Deno does not provide a way to do gethostbyname. In Compose, Docker uses
+      DNS to inform the ip address of other containers. ECS uses /etc/hosts.
+      Therefore this hack to obtain the ip address of the Chrome host.
+      
+      Getting /json/version to retrieve browserWSEndpoint:
+      browserWSEndpoint can be used with either hostname or ip address, the 
+      problem is obtaining it. Deno does not allow Host header to be
+      provided in fetch(), and there aren't other reliable http client
+      implementations.
+    */
     if (Deno.env.get('ECS')) {
       console.log(">>>>>>>>>>>>>>>>>>> ECS");
       const hostsPath = "/etc/hosts";  // Hosts file path
