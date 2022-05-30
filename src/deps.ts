0bfa3c87e4a583b74c93264f38c62dbef56a98f2
Mariano Benedettini <mbenedettini@gmail.com>
2022-05-30 18:26:26 -0300

    Fix oak version

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,5 @@
 import puppeteer from "https://deno.land/x/puppeteer@9.0.2/mod.ts";
-import * as oak from "https://deno.land/x/oak/mod.ts";
+import * as oak from "https://deno.land/x/oak@v9.0.1/mod.ts";
 
 export {
   puppeteer,
